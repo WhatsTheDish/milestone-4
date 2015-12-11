b921cdb3b83a00af078c07f875bc03fe3645fff7
Jeff Howard <jhowa15@u.rochester.edu>
2015-12-11 01:04:50 -0500

    Deez changes

--- server.js
+++ server.js
@@ -61,38 +61,6 @@ db.serialize(function() {
   });
 });
 
-/*
-//DATABASE TAKE 3 for recipes source = http://blog.modulus.io/nodejs-and-sqlite
-var fs = require("fs");
-var file = "recipes.db";
-var exists = fs.existsSync(file);
-
-if(!exists) {
-  console.log("Creating DB file.");
-  fs.openSync(file, "w");
-}
-
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database(file);
-
-db.serialize(function() {
-  if(!exists) {
-    //db.run("CREATE TABLE Stuff (thing TEXT)");
-    db.run("CREATE TABLE recipes(title TEXT primary key, recipe TEXT, allergicToMilk BOOLEAN, allergicToPeanuts BOOLEAN)");
-  }
-  
-  //var stmt = db.prepare("INSERT INTO Stuff VALUES (?)");
-  //var stmt = db.prepare("INSERT INTO users VALUES(?, ?, ?)");
-  //stmt.run('Bob420', false, false);
-  //stmt.finalize();
- 
-  db.each("SELECT rowid AS id, title, recipe, allergicToMilk, allergicToPeanuts FROM recipes", function(err, row) {
-  console.log(row.id + ": " + row.title + " " + row.recipe + " " + row.allergicToMilk + " " + row.allergicToPeanuts);
-  });
-});
-
-*/
-
 //db.close();
 // put all of your static files (e.g., HTML, CSS, JS, JPG) in the static_files/
 // sub-directory, and the server will serve them from there. e.g.,:
@@ -113,30 +81,6 @@ app.use(express.static('static_files'));
 
 // REST API as described in http://pgbovine.net/rest-web-api-basics.htm
 
-/* Run through a full API test session with curl (commands typed after '$',
-   outputs shown on the line below each command) ...
-$ curl -X GET http://localhost:3000/users
-["Philip","Jane","John"]
-$ curl -X GET http://localhost:3000/users/Philip
-{"name":"Philip","job":"professor","pet":"cat.jpg"}
-$ curl -X PUT --data "job=bear_wrangler&pet=bear.jpg" http://localhost:3000/users/Philip
-OK
-$ curl -X GET http://localhost:3000/users/Philip
-{"name":"Philip","job":"bear_wrangler","pet":"bear.jpg"}
-$ curl -X DELETE http://localhost:3000/users/Philip
-OK
-$ curl -X GET http://localhost:3000/users/Philip
-{}
-$ curl -X GET http://localhost:3000/users
-["Jane","John"]
-$ curl -X POST --data "name=Carol&job=scientist&pet=dog.jpg" http://localhost:3000/users
-OK
-$ curl -X GET http://localhost:3000/users
-["Jane","John","Carol"]
-$ curl -X GET http://localhost:3000/users/Carol
-{"name":"Carol","job":"scientist","pet":"dog.jpg"}
-*/
-
 // CREATE a new user
 //
 // To test with curl, run:
@@ -147,9 +91,6 @@ app.post('/users', function (req, res) {
   //window.userPeanutsInfo = postBody.allergicToPeanut;
   //window.userMilkInfo = postBody.allergicToMilk;
 
-  //console.log(Peanuts);
-  //console.log(Milk);
-  ///console.log(myName);
   // must have a name!
   if (!myName) {
     res.send('ERROR');
@@ -180,27 +121,11 @@ app.post('/recipes', function (req, res) {
   var Peanuts = postBody.allergicToPeanut;
   var Milk = postBody.allergicToMilk;
 
-
-  console.log(postBody);
-  console.log(Title);
-  console.log(req);
   // must have a name!
   if (!Title) {
     res.send('ERROR');
     return; // return early!
   }
-  /*
-  // check if user's name is already in database; if so, send an error
-  for (var i = 0; i < db.length; i++) {
-    var e = db[i];
-    if (e.userName == myName) {
-      res.send('ERROR');
-      return; // return early!
-    }
-  }
-  */
-  // otherwise add the user to the database by pushing (appending)
-  // postBody to the fakeDatabase list
   var stmt = db.prepare("INSERT INTO recipes VALUES(?, ?, ?, ?, ?)");
   stmt.run(postBody.title, postBody.creator, postBody.recipe, postBody.allergicToMilk, postBody.allergicToPeanut, function(error){
     if(error){
@@ -213,16 +138,55 @@ app.post('/recipes', function (req, res) {
   }
   );
   stmt.finalize();
-// Updated upstream
-
-// Stashed changes
 });
+
+// DELETE a  recipe
+//
+// To test with curl, run:
+//   curl -X POST --data "name=Carol&job=scientist&pet=dog.jpg" http://localhost:3000/users
+app.delete('/recipes/delete/*', function (req, res) {
+  var recipeToDelete = req.params[0];
+
+  db.all("DELETE * FROM recipes WHERE title = ?", recipeToDelete, function(err, row){
+    if(err){ 
+      throw err;
+    }  
+    else{
+       res.send("OK");
+       return; 
+    }
+  });
+  /*
+  var postBody = req.body;
+  var Title = postBody.title;
+  var Recipe = postBody.recipe;
+  var Peanuts = postBody.allergicToPeanut;
+  var Milk = postBody.allergicToMilk;
+
+  // must have a name!
+  if (!Title) {
+    res.send('ERROR');
+    return; // return early!
+  }
+  var stmt = db.prepare("INSERT INTO recipes VALUES(?, ?, ?, ?, ?)");
+  stmt.run(postBody.title, postBody.creator, postBody.recipe, postBody.allergicToMilk, postBody.allergicToPeanut, function(error){
+    if(error){
+      console.log(error.message);
+      res.send('DUPLICATE');
+    }
+    else{
+      res.send('OK');
+    }
+  }
+  );
+  stmt.finalize();
+  */
+});
+
+
 
 // READ profile data for a user
 //
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
 app.get('/users/*', function (req, res) {
   var nameToLookup = req.params[0];
   console.log("BLAH: " + req.body);
@@ -240,9 +204,6 @@ app.get('/users/*', function (req, res) {
 
 // READ suitable recipes for a user
 //
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
 app.get('/recipes', function (req, res) {
   var milkCheck = req.query.milk; 
   var peanutCheck = req.query.peanuts;
@@ -261,10 +222,7 @@ app.get('/recipes', function (req, res) {
 
 //TITLE SEARCH
 // READ suitable recipes for a user ACCORDING TO SEARCH
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
+
 app.get('/recipes/searchtitle/*', function (req, res) {
   
   var query = req.params[0];
@@ -282,12 +240,7 @@ app.get('/recipes/searchtitle/*', function (req, res) {
 
 });
 
-//AUTHOR SEARCH
-// READ suitable recipes for a user ACCORDING TO SEARCH
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
+//CREATOR SEARCH
 app.get('/recipes/searchcreator/*', function (req, res) {
   
   var query = req.params[0];
@@ -306,11 +259,6 @@ app.get('/recipes/searchcreator/*', function (req, res) {
 });
 
 //CONTENT SEARCH
-// READ suitable recipes for a user ACCORDING TO SEARCH
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
 app.get('/recipes/searchcontent/*', function (req, res) {
   
   var query = req.params[0];
@@ -330,9 +278,6 @@ app.get('/recipes/searchcontent/*', function (req, res) {
 
 
 // UPDATE a user's profile with the data given in POST
-//
-// To test with curl, run:
-//   curl -X PUT --data "job=bear_wrangler&pet=bear.jpg" http://localhost:3000/users/Philip
 app.put('/users/*', function (req, res) {
   var milkChange = req.body.allergicToMilk;
   var peanutChange = req.body.allergicToPeanut;
@@ -421,14 +366,8 @@ app.get('/users', function (req, res) {
 });
 */
 
-
-
-
 // DELETE a user
 //
-// To test with curl, run:
-//   curl -X DELETE http://localhost:3000/users/Philip
-//   curl -X DELETE http://localhost:3000/users/Jane
 app.delete('/users/*', function (req, res) {
   var nameToLookup = req.params[0]; // this matches the '*' part of '/users/*' above
   // try to look up in fakeDatabase
@@ -440,7 +379,6 @@ app.delete('/users/*', function (req, res) {
       return; // return early!
     }
   }
-
   res.send('ERROR'); // nobody in the database matches nameToLookup
 });
 
